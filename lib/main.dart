719c9b97a9d99e6d4ef5992006f19b547f6401b9
HumairaSoftwareEngineer <132704156+HumairaSoftwareEngineer@users.noreply.github.com>
2025-06-20 01:44:02 -0700

    2nd changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/screens/portfolio_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
